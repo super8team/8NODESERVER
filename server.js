f31767604a2768090c8fabfeda9b34cfea23c216
chokyuseung <whrbtmd457@naver.com>
2017-06-14 14:39:53 +0900

    modify child on

--- server.js
+++ server.js
@@ -222,8 +222,9 @@ socket.on('class1', function (data) { // 클라이언트에서 my other event가
    socket.on('kidGPS', function (data) {
      console.log(data);
      console.log("kidGPS IN");
-     //socket.emit('getKidGPS', child);
+     socket.emit('getKidGPS', child);
      socket.broadcast.emit('getKidGPS', child);   //전체클라이언트에 gps 이벤트를 보냅니다.
+     console.log("child 보냈습니다 : " + child);
    });
    socket.on('studentGPS', function (data) {
      console.log(data);
